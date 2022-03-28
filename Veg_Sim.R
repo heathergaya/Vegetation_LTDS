99c85ee673a858f629c16c66c7b8370b204f01ca
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2022-03-28 07:47:45 -0500

    Removed typo, added comments

--- Veg_Sim.R
+++ Veg_Sim.R
@@ -1,27 +1,27 @@
-##This script is set to run remotely via a Linux bash script on a 36 core computer 
+##This script can be set to run remotely via a Linux bash script on a 36 core computer 
 ## Hence the "CommandArgs" lines
 
 #args <- commandArgs(TRUE)
 #Start <- as.numeric(args[1])
 #Stop <- as.numeric(args[2])
-#Start <- c(93, 95)
 
 
-sim.Burrs <- dget(file = 'Thesis_Sim_10182018.txt')
-for (i in Start) {
-  source('RJMCMC.R')
-  require(Rdistance)
+sim.Burrs <- dget(file = 'Thesis_Sim_10182018.txt') #simulated data set
+for (i in Start) { 
+  require(Rdistance) #for comparing w/ maximum likelihood estimation
   library(runjags)
-  library(rjags)
-All.Data <- data.frame(sim.Burrs[[i]]$all.burr)
+All.Data <- data.frame(sim.Burrs[[i]]$all.burr) #grab the simulation data 
 Burrs <- data.frame(dist = All.Data$dist, size = (All.Data$diam)/10, Found = All.Data$found, 
                     Trans = All.Data$trans, Veg = All.Data$known_veg/100, Occ = All.Data$occ)
-Found <- subset(Burrs, Burrs$Found == 1)
+#need distance to transect, size of burrow, if the burrow was detected, what transect (for Rdistance), 
+#the veg information at that burrow and the occupancy of the burrow 
+
+Found <- subset(Burrs, Burrs$Found == 1) #we only work with detected burrows 
 n.transects <- sim.Burrs[[i]]$n.lines
 n <- nrow(Burrs)
-ifelse(n == 200, nz <- 300, nz <- 450)
+ifelse(n == 200, nz <- 300, nz <- 450) #data augmentation 
 
-
+## If you want to compare with Program DISTANCE:
 # require(Rdistance)
 # detection.data <- data.frame(siteID = as.factor(Found$Trans), groupsize = 1, dist = Found$dist)
 # transect.data <- site.data <-  data.frame(siteID = as.factor(c(1:n.transects)), length = sim.Burrs[[i]]$len)
@@ -48,7 +48,7 @@ model
   w[i] ~ dbern(psi)					##augmentation 
   x[i] ~ dunif(0,Bx)					#distance from line for the missed ones; Bx = max(distances) 
   
-  z[i] ~ dgamma(a[i],c.beta[i])T(4,65)		                
+  z[i] ~ dgamma(a[i],c.beta[i])T(5,65)	#burrow sizes	                
   a[i] <- shape[clust[i]]		## a depends on which cluster the size comes from
   c.beta[i] <- betaClust[clust[i]]	#precision is allowed to vary between clusters
   clust[i] ~ dcat( pClust[1:Nclust] )	## clusters are defined as categories, assuming Nclust clusters
@@ -64,24 +64,25 @@ model
   y[i] ~ dbern(mu[i])
   
   o[i]~ dbin(o2[i], 1)
-  logit(o2[i]) <- o.int + z.beta*z[i]						
+  logit(o2[i]) <- o.int + z.beta*z[i]			#larger burrows probably less likely to be occupied		
+  ow[i] <- o[i]*w[i] # was this burrow both occupied and real
   }
   
   for (q in 1:q) {
-  v2[q] ~ dbeta (d,e)T(.05,.99)		# all veg measurements at the site
+  v2[q] ~ dbeta (d,e)T(.05,.99)		# all veg measurements at the site inform the veg at augmented burrows
   }
   
-  p.online ~ dunif(0.4, 0.6)		#estimated detection on line for 5 cm burrows
-  #b.point <- 20
-  b.point ~ dunif(18, 22)
+  p.online ~ dunif(.2, 0.8)		#estimated detection on line for 5 cm burrows
+  #b.point <- 20 #could alternatively set break-point for perfect detection 
+  b.point ~ dunif(15, 25) #prior for size of burrow where perfect detection is reached
   m <- (1-p.online)/(b.point-5) 	#slope for detection on the line for smaller burrows		
   intercept <- p.online-(5*m)	## finding intercept via the detection of the 5cm burrow 
   
   
-  sigma.int~ dnorm(0,s.int.tau)T(0,)
+  sigma.int~ dnorm(0,s.int.tau)
   s.int.tau <- 1/(s.int.sd*s.int.sd)
   s.int.sd ~ dunif(.00001,5)	
-  sigma.beta~ dnorm(0,s.beta.tau)T(0,)
+  sigma.beta~ dnorm(0,s.beta.tau)
   s.beta.tau <- 1/(s.beta.sd*s.beta.sd)
   s.beta.sd ~ dunif(.00001,5)
   o.int~ dnorm(0,o.int.tau)
@@ -90,12 +91,12 @@ model
   z.beta~ dnorm(0,z.beta.tau)
   z.beta.tau <- 1/(z.beta.sd*z.beta.sd)
   z.beta.sd ~ dunif(.00001,5)	
-  sigma.gamma~ dnorm(0,s.gamma.tau)T(,0)
+  sigma.gamma~ dnorm(0,s.gamma.tau)
   s.gamma.tau <- 1/(s.gamma.sd*s.gamma.sd)
   s.gamma.sd ~ dunif(.00001,5)
   
-  d~dunif(.1,40)
-  e~dunif(.1,40)
+  d~dunif(.1,40) #for veg
+  e~dunif(.1,40) # for veg 
   
   for (clustIdx in 1: Nclust) {
   shape[clustIdx] ~ dunif(1,80)
@@ -108,15 +109,20 @@ model
   psi~ dunif(0,1)			#exists or not		
   
   
-  Occ <- sum(o)/(nind+nz)
+  Occ <- sum(wo)/(nind+nz)
   N <- sum(w)	
   D <- N/(2*L*Bx)
   Nt <- N*Occ
   Dt <- Nt/(2*L*Bx)	#tort density
+
+  N <- sum(w)		
+	D <- N/(2*L*Bx)			#burrow density
+	Nt <- sum(wo)			 
+	Dt <- Nt/(2*L*Bx)			#tort density
   
-  juvi1 <- sum(z < 13)/(nind+nz)
-  juvi2 <- sum(z < 20)/(nind+nz)
-  juvi3 <- sum(z < 10)/(nind+nz)
+  juvi1 <- sum(woz < 13)/Nt  
+  juvi2 <-  (sum(woz < 21)- sum(woz <13))/Nt
+  juvi3 <- sum(woz > 21)/Nt
 }
 "
 
@@ -136,25 +142,7 @@ q = length(v2)
 Bx = max(Found$dist, na.rm = TRUE)
 L <- sum(sim.Burrs[[i]]$len)*10^-4
 
-# w.clust <- c(0.50, 0.1, 0.33, .03, .04)
-# shape.clust <- c(10, 20, 30, 40, 66)
-# rate.clust <- c(1,.7,1, 2, 2)
-# y.clust <- Found$size
-# Z <- do.call(cbind, lapply(1:5, function(j)
-#   w.clust[j]*dgamma(y.clust, shape.clust[j], rate = rate.clust[j])))
-# Z <- apply(Z, 1, function(x) which(x==max(x))[1])
-# res <- mixgam.rjmcmc(y = y.clust, nsweep = 80000, kmax = 10, k = 5, w = w.clust, 
-#                      shape = shape.clust, rate = rate.clust, Z,
-#                      delta=1.5, xi=NULL, kappa=NULL, alpha=NULL,
-#                      beta=NULL, g=NULL, h=NULL, verbose=TRUE)
-# 
-# ksave <- res$k.save
-# groups <- round(table(ksave[-(1:40000)])/40000,4)
-# Nclust <- as.numeric(names(groups)[which(groups == max(groups))])
-# if(length(Nclust) != 1){Nclust <- sample(Nclust,1)}
-# if(Nclust < 3){Nclust <- 3}
-
-Nclust <- 4
+Nclust <- 5 #a large number of clusters
 
 clust = rep(NA,(nind +nz)) 	# no idea which cluster anything is in, so unknown	
 clust[which.min(z)]=1 # smallest value assigned to cluster 1; generally represents juvis
@@ -164,15 +152,15 @@ w.i <- c(rep(1, nind), rep(0,nz))
 ### Run the model 
 jd.test2 = list(nind= nind, nz = nz, L = L, Bx = Bx, x=x,y=y,z=z, Nclust = Nclust, q=q, o=o, v=v, 
                 clust= clust, onesRepNclust = rep(1,Nclust))
-ji.test2 <-list(w = w.i, betaClust =c(1.6, .8, 1.25, 1.2), pClust = c(.4, .3, .2, .1),
+ji.test2 <-list(w = w.i, betaClust =c(1.6, .8, 1.25, 1.2, 1.3), pClust = c(.4, .25, .2, .1,.05 ),
                 sigma.beta = .025, sigma.int = 1, sigma.gamma = -1.5,
-                shape = c(52, 9, 65, 55), z.beta = -0.016) 
+                shape = c(52, 9, 65, 55, 60), z.beta = -0.016) 
 jp.test2 <- c("D","Dt", "N","Nt", "betaClust", "pClust","shape","sigma.int", "sigma.beta", "p.online", 
               "b.point", "Occ", "sigma.gamma", "juvi1", "juvi2", "juvi3", "z.beta", "o.int")
 
 Foo2 <- autorun.jags(modelstring.Vegetation, data = jd.test2, inits = list(ji.test2, ji.test2, ji.test2), monitor= jp.test2, 
                      startburnin = 10000, startsample = 40000, adapt = 1000, method = 'parallel', n.chain = 3, max.time="8h", 
-                     psrf.target = 1.2,silent.jags = FALSE, summarise = TRUE)
+                     psrf.target = 1.1,silent.jags = FALSE, summarise = TRUE)
 Covmod2 <- summary(Foo2)
 
 
