fd96598af4f683dd21824eda0b71c7d5e784b9af
Heather Gaya <Heather.e.gaya@gmail.com>
2022-09-07 10:45:59 -0400

    Update Veg_Sim.R

--- Veg_Sim.R
+++ Veg_Sim.R
@@ -1,20 +1,21 @@
 ##This script can be set to run remotely via a Linux bash script on a 36 core computer 
 ## Hence the "CommandArgs" lines
-
-#args <- commandArgs(TRUE)
+#Use the following if you want to run these through Linux:
+#args <- commandArgs(TRUE) 
 #Start <- as.numeric(args[1])
 #Stop <- as.numeric(args[2])
 
 
 sim.Burrs <- dget(file = 'Thesis_Sim_10182018.txt') #simulated data set
-for (i in Start) { 
+Start <- 1 #the first simulation to run
+Stop <- nrow(summary(sim.Burrs)) #the last simulation you want to run
+for (i in Start:Stop) {  #loop across each simulation
   require(Rdistance) #for comparing w/ maximum likelihood estimation
   library(runjags)
 All.Data <- data.frame(sim.Burrs[[i]]$all.burr) #grab the simulation data 
 Burrs <- data.frame(dist = All.Data$dist, size = (All.Data$diam)/10, Found = All.Data$found, 
                     Trans = All.Data$trans, Veg = All.Data$known_veg/100, Occ = All.Data$occ)
-#need distance to transect, size of burrow, if the burrow was detected, what transect (for Rdistance), 
-#the veg information at that burrow and the occupancy of the burrow 
+#need distance to transect, size of burrow, if the burrow was detected, what transect (for Rdistance), the veg information at that burrow and the occupancy of the burrow 
 
 Found <- subset(Burrs, Burrs$Found == 1) #we only work with detected burrows 
 n.transects <- sim.Burrs[[i]]$n.lines
@@ -25,7 +26,7 @@ ifelse(n == 200, nz <- 300, nz <- 450) #data augmentation
 # require(Rdistance)
 # detection.data <- data.frame(siteID = as.factor(Found$Trans), groupsize = 1, dist = Found$dist)
 # transect.data <- site.data <-  data.frame(siteID = as.factor(c(1:n.transects)), length = sim.Burrs[[i]]$len)
-# area = 10000
+# area <- 10000
 # est <- autoDistSamp(dist~1, detection.data, site.data, w.lo=0, w.hi=max(Found$dist),
 #                     likelihoods = c("halfnorm", "uniform", "Gamma"),
 #                     series = c("cosine", "hermite", "simple"), expansions =0:3, warn = TRUE,
@@ -78,20 +79,21 @@ model
   m <- (1-p.online)/(b.point-5) 	#slope for detection on the line for smaller burrows		
   intercept <- p.online-(5*m)	## finding intercept via the detection of the 5cm burrow 
   
+  ## Priors are included below: 
   
-  sigma.int~ dnorm(0,s.int.tau)
+  sigma.int~ dnorm(0,s.int.tau) #intercept of sigma (the shape parameter detection)
   s.int.tau <- 1/(s.int.sd*s.int.sd)
   s.int.sd ~ dunif(.00001,5)	
-  sigma.beta~ dnorm(0,s.beta.tau)
+  sigma.beta~ dnorm(0,s.beta.tau) #relationship of sigma to burrow size
   s.beta.tau <- 1/(s.beta.sd*s.beta.sd)
   s.beta.sd ~ dunif(.00001,5)
-  o.int~ dnorm(0,o.int.tau)
+  o.int~ dnorm(0,o.int.tau) #intercept for burrow occupancy (logit scale)
   o.int.tau <- 1/(o.int.sd*o.int.sd)
   o.int.sd ~ dunif(.00001,5)		
-  z.beta~ dnorm(0,z.beta.tau)
+  z.beta~ dnorm(0,z.beta.tau) #relationship burrow occupancy to size (logit scale)
   z.beta.tau <- 1/(z.beta.sd*z.beta.sd)
   z.beta.sd ~ dunif(.00001,5)	
-  sigma.gamma~ dnorm(0,s.gamma.tau)
+  sigma.gamma~ dnorm(0,s.gamma.tau) #relationship between vegetation and sigma parameter of detection 
   s.gamma.tau <- 1/(s.gamma.sd*s.gamma.sd)
   s.gamma.sd ~ dunif(.00001,5)
   
@@ -105,47 +107,47 @@ model
   
   pClust[1:Nclust] ~ ddirch(onesRepNclust) ## probability of each cluster = the probability of that category in the ddirch distribution
   
-  
   psi~ dunif(0,1)			#exists or not		
   
+  # Derived Quantities
   
-  Occ <- sum(ow)/(nind+nz) #proportion occupied of all indiviudals (real and not)
-  N <- sum(w)	
-  D <- N/(2*L*Bx)
-  Nt <- sum(ow) #all occupied burrows
-  Dt <- Nt/(2*L*Bx)	#tort density
+  Occ <- sum(ow[])/(nind+nz) #proportion occupied of all indiviudals (real and not)
+  N <- sum(w)	 #Burrow Abundance 
+  D <- N/(2*L*Bx) #density of burrows 
+  Nt <- sum(ow[]) #occupied burrow abundance 
+  Dt <- Nt/(2*L*Bx)	#tortoise (occupied burrow) density
   
-  juvi1 <- sum(wo*z < 13)/Nt  
-  juvi2 <-  (sum(wo*z < 21)- sum(wo*z <13))/Nt
-  juvi3 <- sum(wo*z > 21)/Nt
+  juvi1 <- sum(ow*z < 13)/Nt   #proportion burrows < 13 cm wide
+  juvi2 <-  (sum(ow*z < 21)- sum(wo*z <13))/Nt #proportion burrows between 13 and 21 cm wide
+  juvi3 <- sum(ow*z >= 21)/Nt #proportion burrows > 21 cm wide
 }
 "
 
-x <- c(Found$dist, rep(NA, nz))
-y <- c(rep(1,nrow(Found)), rep(0, nz))
-z <- c(Found$size, rep(NA, nz))
-v <- c(Found$Veg, rep(NA, nz))
-v <- ifelse(v < 0.01, .01, v)
-v <- ifelse(v > 0.95, .95, v)
-o <- c(Found$Occ, rep(NA, nz))
-nind <- nrow(Found)
+x <- c(Found$dist, rep(NA, nz)) #distance to transect
+y <- c(rep(1,nrow(Found)), rep(0, nz)) #found (1) or not found (0)
+z <- c(Found$size, rep(NA, nz)) # size of burrows
+v <- c(Found$Veg, rep(NA, nz)) #vegetation obstruction at burrows
+v <- ifelse(v < 0.01, .01, v) #minimize extremes to help with convergence
+v <- ifelse(v > 0.95, .95, v) #minimize extremes to help with convergence
+o <- c(Found$Occ, rep(NA, nz)) #occupancy of burrows 
+nind <- nrow(Found) #how many burrows were observed
 
-v2 <- sim.Burrs[[i]]$Vegcollect/100
-v2 <- ifelse(v2 < 0.01, .01, v2)
-v2 <- ifelse(v2 > 0.95, .95, v2)
-q = length(v2)
-Bx = max(Found$dist, na.rm = TRUE)
-L <- sum(sim.Burrs[[i]]$len)*10^-4
+v2 <- sim.Burrs[[i]]$Vegcollect/100 #vegetation everywhere at the site, not just at burrows 
+v2 <- ifelse(v2 < 0.01, .01, v2) #minimize extremes to help with convergence
+v2 <- ifelse(v2 > 0.95, .95, v2) #minimize extremes to help with convergence
+q <- length(v2) #how many veg points were taken
+Bx <- max(Found$dist, na.rm = TRUE) #maximum distance from transect to consider in abundance ests
+L <- sum(sim.Burrs[[i]]$len)*10^-4 #length of transects
 
 Nclust <- 5 #a large number of clusters
 
-clust = rep(NA,(nind +nz)) 	# no idea which cluster anything is in, so unknown	
-clust[which.min(z)]=1 # smallest value assigned to cluster 1; generally represents juvis
-clust[which.max(z)]=Nclust # highest value assigned to largest cluster value; generally represents large adults
-w.i <- c(rep(1, nind), rep(0,nz))
+clust <- rep(NA,(nind +nz)) 	# no idea which cluster anything is in, so unknown	
+clust[which.min(z)] <- 1 # smallest value assigned to cluster 1; generally represents juvis
+clust[which.max(z)] <- Nclust # highest value assigned to largest cluster value; generally represents large adults
+w.i <- c(rep(1, nind), rep(0,nz)) #initialize burrows as real/not
 
 ### Run the model 
-jd.test2 = list(nind= nind, nz = nz, L = L, Bx = Bx, x=x,y=y,z=z, Nclust = Nclust, q=q, o=o, v=v, 
+jd.test2 <- list(nind= nind, nz = nz, L = L, Bx = Bx, x=x,y=y,z=z, Nclust = Nclust, q=q, o=o, v=v, v2 = v2, q= q,
                 clust= clust, onesRepNclust = rep(1,Nclust))
 ji.test2 <-list(w = w.i, betaClust =c(1.6, .8, 1.25, 1.2, 1.3), pClust = c(.4, .25, .2, .1,.05 ),
                 sigma.beta = .025, sigma.int = 1, sigma.gamma = -1.5,
@@ -160,7 +162,7 @@ Covmod2 <- summary(Foo2)
 
 
 
-results = list()
+results <-  list()
 results$simulation <- paste("Dataset #", i)
 results$true <- c(paste('Truth'), n/(1000*1000*10^-4))
 results$DISTANCE <- c(paste('Distance'), ci.est.null, pt.est.null)
